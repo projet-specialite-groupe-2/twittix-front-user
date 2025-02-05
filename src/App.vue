9e22a695d5d98703c7edab3b0ca6b10e6acad945
SwannBali <swannbld@gmail.com>
2025-02-05 12:12:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main id="body-block">
+    <router-view id="content_page" v-slot="{ Component }">
+      <component :is="Component" />
+    </router-view>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
